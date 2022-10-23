7138a70022a15c8daa0a212f2c4e81bbcff74717
Zeev Ginzburg <109742174+ZeevGinzburg@users.noreply.github.com>
2022-10-23 20:15:32 +0300

    Reading the data from the server creating a loop to show the name and symbol still with a static URL

--- JS/stockSearchMain.js
+++ JS/stockSearchMain.js
@@ -1,23 +1,41 @@
-const basicUrl = "https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/profile/AAPL";
+const exmpUrl = "https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/search?query=AA&limit=10&exchange=NASDAQ";
+
+// const basicUrl = "https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/";
+
+// const searchQuery = "search?query=AA&limit=10&exchange=NASDAQ"
+// const serchUrl = basicUrl + searchQuery;
 
 async function askingFreeStockServerRespone(url) {
-    // console.log("im working")
-    try {const freeStockData = await fetch(url);
-    const jsonFreeStockData = await freeStockData.json();
-    return jsonFreeStockData.result;}
+    try {
+        const freeStockData = await fetch(url);
+        const jsonFreeStockData = await freeStockData.json();
+    
+    return jsonFreeStockData;
+    }
     catch(error) {
        return "Server error"
     }
 };
 
+async function retrieveCompanyNameAndSymbol(object) {
+    const symbolOfFirstObject = object.symbol;
+    const companyNameOfFirstObject = object.name;
+    const companyNameandSymbol = `${companyNameOfFirstObject} (${symbolOfFirstObject})`;
+    return companyNameandSymbol;
+
+}
+
 async function presentOutput(url) {
-    const searchResultList = document.createElement("li");
-    const stockData = await askingFreeStockServerRespone(url);
-    const searchResultText = document.createTextNode(stockData);
-    searchResultList.appendChild(searchResultText);
-    const element = document.getElementById("result-list");
-    element.appendChild(searchResultList);
-    console.log("presentout working")
+    const stockArray = await askingFreeStockServerRespone(url); 
+    for (let i = 0; i <= stockArray.length -1; i++) {
+        const companyNameandSymbol = await retrieveCompanyNameAndSymbol(stockArray[i]);
+        const searchResultList = document.createElement("li");
+        const searchResultText = document.createTextNode(companyNameandSymbol);
+        searchResultList.appendChild(searchResultText);
+        const element = document.getElementById("result-list");
+        element.appendChild(searchResultList);
+    }
+
 }
 
 
@@ -26,5 +44,6 @@ async function presentOutput(url) {
 document.getElementById("searchButton").addEventListener("click",function(event) {searchButton()});
 
 function searchButton() {
-    presentOutput(basicUrl);
+    presentOutput(exmpUrl);
+
 }
